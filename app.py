1374255a215275b5b353b0c92c58784ffd36c934
jaychandra6 <jaypatel6814@gmail.com>
2021-02-15 18:49:44 +0530

    added getMore() function that fetches more than one meme

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return jsonify(get.main())
 
+@app.route("/<id>")
+def getMore(id):
+    return jsonify(get.main(limit=int(id)))
+
 if __name__ == "__main__":
     app.run(debug=True)
